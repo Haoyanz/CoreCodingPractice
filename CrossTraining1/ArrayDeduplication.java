3512f5fcd3137dec2ae5c9130cbb8a82ab7f2032
Haoyanz <49219731+Haoyanz@users.noreply.github.com>
2019-08-21 22:55:51 -0700

    ArrayDeduplication 1

--- CrossTraining1/ArrayDeduplication.java
+++ CrossTraining1/ArrayDeduplication.java
@@ -1,3 +1,11 @@
+/*
+Given a sorted integer array, remove duplicate elements. For each group of elements with the same value keep only one of them. Do this in-place, using the left side of the original array and maintain the relative order of the elements of the array. Return the array after deduplication.
+
+Assumptions : The array is not null
+
+Examples : {1, 2, 2, 3, 3, 3} → {1, 2, 3}
+*/
+
 public class Solution {
   public int[] dedup(int[] array) {
     //when array length is 0 or 1, return input array directly
