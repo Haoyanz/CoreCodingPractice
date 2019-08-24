e147f628f7a41ebae1fb9625fd054626b36a06de
Haoyanz <49219731+Haoyanz@users.noreply.github.com>
2019-08-24 12:44:34 -0700

    Check If Linked List Has A Cycle

---
+++ Linked Lists Basic/Check If Linked List Has A Cycle.java
@@ -0,0 +1,39 @@
+/*
+Check if a given linked list has a cycle. Return true if it does, otherwise return false.
+
+ 
+
+Assumption:
+
+You can assume there is no duplicate value appear in the linked list.
+*/
+
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public boolean hasCycle(ListNode head) {
+    if(head == null){
+      return false;
+    }
+    ListNode slow = head;
+    ListNode fast = head;
+    while(fast.next != null && fast.next.next != null){
+      slow = slow.next;
+      fast = fast.next.next;
+      if(slow == fast){
+        return true;
+      }
+    }
+    return false;
+  }
+}
+//Time Complexity : O(n)
+//Space Complexity : O(1)
